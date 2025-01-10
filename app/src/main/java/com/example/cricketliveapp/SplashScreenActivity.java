af46e57099d67c9bcf6a3937c9264188c940c2cd
riya <riyasushma2019@gmail.com>
2025-01-10 17:40:13 +0530

    add splash animation

--- app/src/main/java/com/example/cricketliveapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/cricketliveapp/SplashScreenActivity.java
@@ -1,6 +1,8 @@
 package com.example.cricketliveapp;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.Path;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -12,6 +14,10 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.UnderlineSpan;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.navigation.NavController;
@@ -22,19 +28,54 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.cricketliveapp.databinding.ActivitySplashScreenBinding;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    ImageView logo, logo_bg, ball;
+    Animation rotation, rotate;
+    ObjectAnimator objectAnimator, alphaanimationball, alpahaanimationlogo_bg, alphaanimationlogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 20000);
+        ball = findViewById(R.id.ball);
+        logo_bg = findViewById(R.id.logo_bg);
+        logo = findViewById(R.id.logo);
+
+//        logo_bg.setBackgroundResource(R.drawable.gradientopacity);
+
+        float centerX = -110;
+        float centerY = -110;
+        float radius = 150;
+
+        Path path = new Path();
+        path.addCircle(centerX, centerY, radius, Path.Direction.CCW);
+
+        objectAnimator = ObjectAnimator.ofFloat(ball, "translationX", "translationY", path);
+        objectAnimator.setDuration(2000);
+        objectAnimator.setRepeatCount(ObjectAnimator.INFINITE);
+        objectAnimator.setRepeatMode(ObjectAnimator.RESTART);
+
+        objectAnimator.start();
+
+        alphaanimationball = ObjectAnimator.ofFloat(ball, "alpha", 1f, 0f);
+        alphaanimationball.setDuration(2700);
+        alphaanimationball.start();
+
+        alpahaanimationlogo_bg = ObjectAnimator.ofFloat(logo_bg, "alpha", 1f, 0f);
+        alpahaanimationlogo_bg.setDuration(3000);
+        alpahaanimationlogo_bg.start();
+
+        alphaanimationlogo = ObjectAnimator.ofFloat(logo,  "alpha", 0f, 1f);
+        alphaanimationlogo.setDuration(5000);
+        alphaanimationlogo.start();
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        }, 20000);
     }
 }
